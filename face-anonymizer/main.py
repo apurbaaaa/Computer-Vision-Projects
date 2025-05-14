60de6be1652e95820cef9404c4f72b67197de209
apurbaaaa <apurbakoirala33@gmail.com>
2025-05-14 10:17:18 +0545

    feat: blur impl

--- face-anonymizer/main.py
+++ face-anonymizer/main.py
@@ -1,25 +1,18 @@
-# read image 
+#read image 
 import cv2
 import os
 import mediapipe as mp
-
 img_path = './data/testImg.png'
-if not os.path.exists(img_path):
-    raise FileNotFoundError(f"Image not found at {img_path}")
-
 img = cv2.imread(img_path)
-if img is None:
-    raise ValueError(f"cv2.imread could not read the image at {img_path}")
-
-# detect faces
+#detect faces
 H, W, _ = img.shape
 mp_face_detection = mp.solutions.face_detection
 
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confidence=0.5) as face_detection:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
-
     if out.detections is not None:
+
         for detection in out.detections:
             location_data = detection.location_data
             bbox = location_data.relative_bounding_box
@@ -27,11 +20,12 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
             x1, y1, width, height = bbox.xmin, bbox.ymin, bbox.width, bbox.height
             x1 = int(x1 * W)
             y1 = int(y1 * H)
-            w = int(width * W)   # ✅ Corrected
-            h = int(height * H)  # ✅ Corrected
+            w = int(width * W)
+            h = int(height * H)
 
-            cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
-
+            #blur faces
+            img[y1:y1+h, x1:x1+h, :] = cv2.blur(img[y1:y1+h, x1:x1+h, :], (40, 40))
     cv2.imshow('img', img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+
+#save image
