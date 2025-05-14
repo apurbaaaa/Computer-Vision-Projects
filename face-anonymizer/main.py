71efbc8d37b832c055caa5130d30a3846c5fd782
apurbaaaa <apurbakoirala33@gmail.com>
2025-05-14 23:04:39 +0545

    feat: updated video blur impl

--- face-anonymizer/main.py
+++ face-anonymizer/main.py
@@ -1,19 +1,14 @@
-#read image 
 import cv2
 import os
 import mediapipe as mp
-data = './data'
-img_path = './data/testImg.png'
-img = cv2.imread(img_path)
-#detect faces
-H, W, _ = img.shape
-mp_face_detection = mp.solutions.face_detection
+import argparse
 
-with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confidence=0.5) as face_detection:
+def process_img(img, face_detection):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
-    if out.detections is not None:
+    H, W, _ = img.shape
 
+    if out.detections is not None:
         for detection in out.detections:
             location_data = detection.location_data
             bbox = location_data.relative_bounding_box
@@ -24,10 +19,43 @@ with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confiden
             w = int(width * W)
             h = int(height * H)
 
-            #blur faces
-            img[y1:y1+h, x1:x1+h, :] = cv2.blur(img[y1:y1+h, x1:x1+h, :], (40, 40))
-    # cv2.imshow('img', img)
-    # cv2.waitKey(0)
+            # blur faces
+            img[y1:y1+h, x1:x1+w, :] = cv2.blur(img[y1:y1+h, x1:x1+w, :], (40, 40))
 
-#save image
-cv2.imwrite(os.path.join(data, 'output_blur.png'), img)
+    return img
+
+args = argparse.ArgumentParser()
+args.add_argument("--mode", default='video')
+args.add_argument("--filePath", default='./data/testVideo.mp4')
+args = args.parse_args()
+
+data = './data'
+
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+    if args.mode == "image":
+        img = cv2.imread(args.filePath)
+        img = process_img(img, face_detection)
+        cv2.imwrite(os.path.join(data, 'output_blur.png'), img)
+
+    elif args.mode == "video":
+        cap = cv2.VideoCapture(args.filePath)
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Failed to read the video.")
+            exit()
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        output_path = os.path.join(data, 'output_video.mp4')
+        output_video = cv2.VideoWriter(output_path, fourcc, 25, (frame.shape[1], frame.shape[0]))
+
+        while ret:
+            processed_frame = process_img(frame, face_detection)
+            output_video.write(processed_frame)
+            ret, frame = cap.read()
+
+        cap.release()
+        output_video.release()
+        print(f"Saved processed video to {output_path}")
