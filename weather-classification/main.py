b6acb83549ad7ea475fb799895e1e6004524a19c
apurbaaaa <apurbakoirala33@gmail.com>
2025-05-22 14:36:44 +0545

    feat: data prep and feature extraction

---
+++ weather-classification/main.py
@@ -0,0 +1,33 @@
+import os
+from img2vec_pytorch import Img2Vec
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from PIL import Image
+#import data and prepare
+img2vec = Img2Vec()
+
+data_dir = './data/'
+train_dir = os.path.join(data_dir, 'train')
+test_dir = os.path.join(data_dir, 'test')
+
+data = {}
+for j, dir_ in enumerate([train_dir, test_dir]):
+    features = []
+    labels = []
+    for category in os.listdir(dir_):
+        for img_path in os.listdir(os.path.join(dir_, category)):
+            img_path_ = os.path.join(dir_, category, img_path)
+            img = Image.open(img_path_)
+
+            img_features = img2vec(img)
+
+            features.append(img_features)
+            labels.append(category)
+
+    data[['training_data', 'validation_data'][j]] = features
+    data[['training_labels', 'validation_labels'][j]] = lables
+#train model
+
+#test model
+
+#save model
